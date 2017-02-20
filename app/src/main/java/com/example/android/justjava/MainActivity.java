73931391cf9190c60579f802bdb5b1723c8c327d
mbeevor@gmail.com <Bourton8113>
2017-02-20 22:38:48 +0000

    Making the quantity picker work

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +28,16 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
         display (quantity);
         displayPrice (quantity * 5);
     }
+
+
 
     /**
      * This method is called when the increment button is clicked.
      */
     public void increment(View view) {
-        int quantity = 2;
         quantity = quantity + 1;
         display (quantity);
     }
@@ -44,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the decrement button is clicked.
      */
     public void decrement(View view) {
-        int quantity = 2;
         quantity = quantity - 1;
         display (quantity);
     }
